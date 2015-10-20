e4f9ef59cf06cd98350d321b45b497651118be55
Yustynn <yustynn@gmail.com>
2015-10-20 14:35:38 +0800

    Finish "Creating a Simple Server in Node.js"

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 // Problem: From web browser, need to look at user's 1) badge count 2) JS points
 
+// Modules
+var http = require('http');
+
+
 // 1. Create a web server
+http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  setInterval(function() {
+    res.write(new Date() + '\n');
+  }, 1000);
+  // res.end('Hello World\n');
+}).listen(3000);
+console.log('Server running at http://localhost:3000/');
 
 // 2. Handle HHTP route GET / and POST / i.e. Home
   // if url == '/' && GET
